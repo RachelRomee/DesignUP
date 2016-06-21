e4813d7f54ba3f8363e806c261b2629dad5b4320
ramonavic <ramon.hofmeister@gmail.com>
2016-06-21 21:09:58 +0200

    uploader + post create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,28 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+
+   def new
+      @post = Post.new
+   end
+
+   def create
+      post = Post.new( post_params )
+
+      if post.save
+         redirect_to posts_path
+      else
+         render 'new'
+      end
+   end
+
+  private
+
+  def post_params
+    params.require( :post ).permit( :title, :description, :image )
+  end
+
+  Post.create(title: "Cuba Sunrise", image: "cubasunrise.jpg", description: "Illustration of a car in the sunrise", user_id: jona.id)
+
 end
