78452118a5b4d1b8a3f73c09d6cd182d571bd522
Rachel Romee <RachelRomee@Rachels-MacBook-Pro.local>
2016-06-23 16:36:55 +0200

    follow statistics, partials, routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,48 +11,52 @@ class PostsController < ApplicationController
   end
 
 
-   def new
-      @post = Post.new
-      @post.user_id = params[:user_id]
-   end
+ def new
+    @post = Post.new
+    @post.user_id = params[:user_id]
+ end
 
-   def create
-      post = Post.new( post_params )
-      @post.user = current_user
+ def create
+    post = Post.new( post_params )
+    @post.user = current_user
 
-      if post.save
-         redirect_to posts_path
-      else
-         render 'new'
-      end
-   end
-
-   def edit
-     @post = Post.find( params[:id] )
-   end
-
-   def update
-     @post = Post.find( params[:id] )
-
-     if @post.update_attributes( post_params )
-       redirect_to @post
-     else
-       render 'edit'
-     end
-   end
-
-   def user
-     @user = User.find( params[:user_id] )
-
-     @posts = Post.where( user: @user ).order( created_at: :desc )
-
-     @likes = @user.likes.joins( :post ).order( "posts.created_at DESC" )
-  end
-
-    private
-
-    def post_params
-      params.require( :post ).permit( :title, :description, :image, :user_id )
+    if post.save
+       redirect_to posts_path
+    else
+       render 'new'
     end
+ end
+
+ def edit
+   @post = Post.find( params[:id] )
+ end
+
+ def update
+   @post = Post.find( params[:id] )
+
+   if @post.update_attributes( post_params )
+     redirect_to @post
+   else
+     render 'edit'
+   end
+ end
+
+ def user
+   @user = User.find( params[:user_id] )
+
+   @posts = Post.where( user: @user ).order( created_at: :desc )
+
+   @likes = @user.likes.joins( :post ).order( "posts.created_at DESC" )
+end
+
+def current_user?(user)
+   user == current_user
+end
+
+private
+
+  def post_params
+    params.require( :post ).permit( :title, :description, :image, :user_id )
+  end
 
 end
