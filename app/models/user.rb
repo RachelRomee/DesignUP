196144e176faa9dedb8bd23c829d7febb0330fb1
Rachel Romee <RachelRomee@Rachels-MacBook-Pro.local>
2016-06-23 15:34:50 +0200

    Relationship model, relationships and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,12 @@ class User < ActiveRecord::Base
   has_one :profile
   has_many :posts
   has_many :comments
-  has_many :likes 
+  has_many :likes
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  # destroying a user should destroy follow relationship
+                                  dependent: :destroy
+
+# validate name column in user table
+  validates :name, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
 
-
   root 'site#index'
+  
   devise_for :users
   resources :profiles
 
