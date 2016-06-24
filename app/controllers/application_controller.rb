912812fbc7f259b92c8bdef1f7a264a02faca27b
ramonavic <ramon.hofmeister@gmail.com>
2016-06-24 17:16:30 +0200

    Permit name on registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
 
 
@@ -9,6 +10,11 @@ class ApplicationController < ActionController::Base
 
     def after_sign_in_path_for(resource)
       posts_path
+    end
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) << :name
     end
 
 end
