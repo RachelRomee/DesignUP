998a433a7107a41517551dc4cc299ece8955b140
ramonavic <ramon.hofmeister@gmail.com>
2016-06-24 16:31:28 +0200

    broken test

--- spec/feature/test_spec.rb
+++ spec/feature/test_spec.rb
@@ -1,4 +1,28 @@
 require 'rails_helper'
 
-feature 'Description of the feature', js: true do
+def login
+  User.create( email: 'stefan@test.com', password: 'password' )
+  visit new_user_session_path
+  sleep(1)
+
+  fill_in 'Email', with: 'stefan@test.com'
+  fill_in 'Password', with: 'password'
+  sleep(1)
+
+  click_button 'Log in'
+end
+
+feature 'Manage tasks', js: true do
+  scenario 'submit post' do
+
+    login
+    # Point your browser towards the todo path
+    visit new_post_path
+    # Enter description in the text field
+    fill_in 'post_title', with: 'Be Batman'
+    # Press enter (to submit the form)
+    page.execute_script("$('form').submit()")
+    # Expect the new task to be displayed in the list of tasks
+    expect(page).to have_content('Be Batman')
+  end
 end
