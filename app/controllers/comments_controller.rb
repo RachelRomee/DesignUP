53bf5f0c540b16d068ab0ae56484102d8d3fd539
ramonavic <ramon.hofmeister@gmail.com>
2016-07-03 14:58:59 +0200

    ajax comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,19 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
-
-  before_action :set_comment, only: [:show, :edit, :update, :destroy]
+    before_action :set_comment, only: [:show, :edit, :update, :destroy]
 
 
   def create
      post = Post.find( params[:post_id] )
+     @comment = Comment.new(post: post, user: current_user, comment: params[:comment] )
 
-     comment = Comment.new(post: post, user: current_user, comment: params[:comment] )
-
-     comment.save
-
-     redirect_to post
+    if  @comment.save
+      render 'create.js.erb'
+    #  respond_to do |format|
+    #    format.html {redirect_to post }
+    #    format.js #render create.js.erb
+    else
+      redirect_to post_path
+    end
   end
 end
